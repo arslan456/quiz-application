b0bf419404de65eef4f05ddabb41e20ca8d68407
arslan456 <muhammadarslanjabbar@gmail.com>
2020-08-18 16:55:53 +0500

    Initialize project using Create React App

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles.d';
+
+type Props={
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNum: number;
+    totalQuestions: number;
+}
+
+export const QuestionCard:React.FC<Props> = ({question,answers,callback,userAnswer,questionNum,totalQuestions}) => {
+    return (
+        <Wrapper>
+            <p>
+                Question: {questionNum} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{__html: question}}/>
+            <div>
+                {answers.map(answer=>(
+                    <ButtonWrapper
+                    correct = {userAnswer?.correctAnswer === answer}
+                    userClicked= {userAnswer?.answer === answer}
+                    >
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{__html:answer}}/>
+                        </button>
+                    </ButtonWrapper>
+                ))}
+                
+            </div>  
+        </Wrapper>
+    )
+}
